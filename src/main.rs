1e13cfd345c25f7baa1b00fbc52002351f32f6a8
ovier-oh <ovier.obregon.hin@outlook.com>
2026-04-17 01:01:49 +0300

    Strucuts data
    Create a struct for task,
    it include id, title, comments, completed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+struct Task {
+    id: u32,
+    title: String,
+    completed: bool,
+}
+
 fn main() {
-    println!("Hello, world from Rust and Im ovier greetings");
+    let task = Task {
+        id: 1,
+        title: String::from("Lavar Trastos"),
+        completed: false,
+    };
+
+    println!("Id: {}", task.id);
+    println!("Title: {}", task.title);
+    println!("Completed: {}", task.completed);
 }
