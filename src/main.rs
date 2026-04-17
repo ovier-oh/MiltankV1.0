aaa2b8c7ef01c2ef66232011223259f78fe9b28c
ovier-oh <ovier.obregon.hin@outlook.com>
2026-04-18 01:36:16 +0300

    Create input for create a new struct

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,42 @@
+use std::io;
+
 struct Task {
     id: u32,
     title: String,
     completed: bool,
+    comments: String,
+}
+
+impl Task {
+    fn new(id: u32, title: String, comments: String) -> Task {
+        Task {
+            id,
+            title,
+            completed: false,
+            comments,
+        }
+    }
 }
 
 fn main() {
-    let task = Task {
-        id: 1,
-        title: String::from("Lavar Trastos"),
-        completed: false,
-    };
+    println!("Enter Id:");
+    let mut id_input = String::new();
+    io::stdin().read_line(&mut id_input).unwrap();
+    let id: u32 = id_input.trim().parse().unwrap();
+
+    println!("Enter title:");
+    let mut title = String::new();
+    io::stdin().read_line(&mut title).unwrap();
+
+    println!("Enter comments:");
+    let mut comments = String::new();
+    io::stdin().read_line(&mut comments).unwrap();
+
+    let task = Task::new(id, title, comments);
 
     println!("Id: {}", task.id);
     println!("Title: {}", task.title);
     println!("Completed: {}", task.completed);
+    println!("Comments: {}", task.comments);
 }
+
