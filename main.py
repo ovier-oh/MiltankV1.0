a64b55d55385d22ce5de32c38c50989d3c591acb
ovier-oh <ovier.obregon.hin@outlook.com>
2026-05-23 13:04:42 +0300

    Add Histogram
    separate describecolumn in:
    1 -> get numeric series
    2 -> describe column
    
    add normaly_test
    add plot histogram

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas as pd 
+import matplotlib.pyplot as plt 
+from scipy.stats import shapiro 
+from typing import cast 
 
 def load_csv(path: str) -> pd.DataFrame:
     return pd.read_csv(path)
@@ -8,9 +11,12 @@ def show_columns(df: pd.DataFrame) -> None:
     for i, col in enumerate(df.columns):
         print(f"[{i}] {col}")
 
-def describe_column(df: pd.DataFrame, column: int) -> None: 
-    series = pd.to_numeric(df[column], errors = "coerce").dropna() 
 
+def get_numeric_series(df: pd.DataFrame, column: str) -> pd.Series:
+    s = cast(pd.Series, df[column])
+    return pd.to_numeric(s, errors="coerce").dropna()
+
+def describe_column(series: pd.Series, column: str) -> None: 
     print("\nSummary Stadistics")
     print("--------------------")
     print(f"Column: {column}")
@@ -23,6 +29,35 @@ def describe_column(df: pd.DataFrame, column: int) -> None:
     print(f"Median: {series.median():.4f}")
     print(f"P75: {series.quantile(0.75):.4f}")
 
+def plot_histogram(series: pd.Series, column: str) -> None:
+    plt.hist(series, bins=8, edgecolor="black")
+    plt.title(f"Histograma - {column}")
+    plt.xlabel(column)
+    plt.ylabel("Frequency")
+    plt.grid(True)
+    plt.show()
+
+
+def normality_test(series: pd.Series) -> None:
+    print("\nShapiro-Wilk normality Test")
+    print("-----------------------------")
+
+    if len(series) < 3:
+        print("There aren't enough values for test")
+        return 
+
+    statistic, p_value = shapiro(series)
+
+    print(f"Statistics: {statistic:.4f}")
+    print(f"P-value: {p_value:.4f}")
+
+    alpha = 0.05 
+
+    if p_value > alpha: 
+        print("Result: Data show a behaviour normal distribution")
+    else:
+        print("Result: Data show a behaviour no normal distribuiton")
+
 
 def main():
     path = input("Path of CSV: ").strip() 
@@ -31,9 +66,13 @@ def main():
     show_columns(df) 
 
     index = int(input("\nChoose numer of column: "))
-    column = df.columns[index]
+    column: str = cast(str, df.columns[index])
 
-    describe_column(df, column)
+    series = get_numeric_series(df, column)
+
+    describe_column(series, column)
+    normality_test(series)
+    plot_histogram(series, column)
 
 if __name__ == "__main__":
     main() 
